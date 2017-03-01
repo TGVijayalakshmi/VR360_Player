731f6c5433719f77c0257d44f1ebb011376de9c7
Vijayalakshmi <vijayalakshmi@Vijayalakshmis-MacBook-Pro.local>
2017-03-01 22:19:30 +0530

    Initial Commit

---
+++ Assets/VideoPlayerUIController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class VideoPlayerUIController : MonoBehaviour 
+{
+	public GameObject PlayButtonGameObject;
+	public GameObject PauseButtonGameObject;
+	public GameObject StopButtonObject;
+
+	private float _gazeHoldTimer = 2f;
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}   
+
+	//Play
+	public void HandleOnPlayEnter()
+	{
+		
+
+	}
+
+	//Pause
+	public void HandleOnPauseEnter()
+	{
+		
+	}
+
+	//Stop
+	public void HandleOnStopEnter()
+	{
+		
+	}
+
+
+}
